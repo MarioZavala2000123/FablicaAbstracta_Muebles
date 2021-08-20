0af0ea7b2e34c5db189bcdf9bf7a5ec0b4b4c566
MarioZavala2000123 <mrzavalat@alumno.uspg.edu.gt>
2021-08-19 18:08:40 -0600

    Silla Moderna

---
+++ src/main/java/Model/SillaModerna.java
@@ -0,0 +1,20 @@
+package Model;
+
+import Interface.iSilla;
+
+public class SillaModerna implements iSilla {
+
+	@Override
+	public void tienePatas() {
+		System.out.println("Silla moderna tiene una base solida para las patras");
+		
+	}
+
+	@Override
+	public void sentarse() {
+		System.out.println("Silla moderna tiene base solida para sentarse");
+		
+	}
+	
+
+}
